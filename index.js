471d5be8a29657702df6e5fd39219bca49481f3b
PaulDev97 <paul.devc@gmail.com>
2022-12-28 17:35:20 -0300

    query

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
+import { guitars } from "./store.js";
+const productsGuitars = guitars; 
 const containerRender = document.querySelector('.cards')
+const btnCategoryContainer = document.querySelector('.filter_btn')
+const btnCategory = document.querySelectorAll('.btn_scroll')
+console.log(productsGuitars)
 
-
-const guitars = [
+/* const guitars = [
   {
     id:1,
-    nombre: 'Gibson',
+    category:'electric',
+    top:'top',
+    brand: 'Gibson',
     img: './img/gibson.png',
-    precio: 400,
+    price: 400,
   },
 
   {
     id:2,
-    nombre: 'Fender',
+    category:'acustic',
+    top:'top',
+    guitar:'acustica',
+    brand: 'Fender',
     img: './img/fender.png',
-    precio: 500,
+    price: 500,
   },
   {
     id:3,
-    nombre: 'Ibanez',
+    top:'top',
+    category:'classic',
+    guitar:'clasica',
+    brand: 'Ibanez',
     img: './img/ibanez.png',
-    precio: 500,
-  }
-]
+    price: 500,
+  },
+ 
+] */
 
 
 const createGuitar = guitar => {
-  const {nombre, img, precio} = guitar;
+  const {brand, img, price} = guitar;
 
   return `
   <div class="guitar_card">
@@ -35,8 +48,8 @@ const createGuitar = guitar => {
 
     <div class="guitar_data_container">
       <div class="guitar_data">
-        <h3>${nombre}</h3>
-        <span>${precio}</span>
+        <h3>${brand}</h3>
+        <span>${price}</span>
       </div>
       <i class="fa-solid fa-circle-info"></i>
     </div>
@@ -50,16 +63,81 @@ const createGuitar = guitar => {
   `
 }
 
-const renderGuitar = guitar => {
-  containerRender.innerHTML = guitar.map(item => createGuitar(item)).join('')
+
+
+/* ----------------------------------------------------------------------------------------- */
+/* -----------------------Logica de filtrar categoria-------------------------------------- */
+/* ----------------------------------------------------------------------------------------- */
+
+/* Evento si clickea x boton y pasarle el elemento a la funcion getDataSetBtn para obtener la categoria y asi darle el background black a ese boton */
+ const btnSelected = (e) => {
+
+  if(!e.target.classList.contains('btn_scroll')) return;
+  
+  getDataSetBtn(e)
+  renderGuitars(e.target.dataset.category)
 }
 
+
+
+const getDataSetBtn = (e) => {
+
+  const dataSetBtn = e.target.dataset.category;
+
+  btnColorSelected(dataSetBtn)
+
+  
+}
+
+
+const btnColorSelected = selected => {
+
+  const getCategoryBtn = [...btnCategory];
+
+  //Recorro cada boton y le saco el active a los que son diferente al seleccionado
+  //si es la categoria que no tengo seleccionada le saco el active
+  //Si es la que seleccione entonces le agrego la clase
+  getCategoryBtn.forEach((item) => {
+    if(item.dataset.category !== selected){
+      item.classList.remove('active');
+      return
+    }
+
+    item.classList.add('active')
+  })
+}
+/* ----------------------------------------------- */
+
+const renderGuitars = (guitar) => {
+  if(guitar === 'popular'){
+    popularGuitars()
+  }
+  else{
+    const guitarCategory = productsGuitars.filter(item => item.category === guitar)
+    containerRender.innerHTML = guitarCategory.map(createGuitar).join('')
+  }
+
+  
+}
+
+
+
+const popularGuitars = () => {
+  const populares = productsGuitars.filter(item => item.top === 'top' )
+  console.log(populares)
+
+  containerRender.innerHTML = populares.map(createGuitar).join('')
+}
 
 
 
 const init = () => {
 
-  renderGuitar(guitars)
+  
+  popularGuitars()
+  btnCategoryContainer.addEventListener('click',btnSelected)
+
+  
 
 }
 
--- store.js
+++ store.js
@@ -1,7 +1,53 @@
-const funkos = [
+export const guitars = [
   {
-    nombre: 'Messi',
-    img: './img/messi.jpg',
-    precio: 400,
-  }
+    id:1,
+    category:'electric',
+    top:'top',
+    brand: 'Gibson',
+    img: './img/gibson.png',
+    price: 400,
+  },
+
+  {
+    id:2,
+    category:'electric',
+    top:'top',
+    brand: 'Fender',
+    img: './img/fender.png',
+    price: 500,
+  },
+  {
+    id:3,
+    top:'top',
+    category:'electric',
+    brand: 'Ibanez',
+    img: './img/ibanez.png',
+    price: 500,
+  },
+ {
+    id:4,
+    top:'top',
+    category:'electric',
+    brand: 'Esp LTD',
+    img: './img/esp.png',
+    price: 500,
+  },
+  {
+    id:5,
+    top:'top',
+    category:'electric',
+    brand: 'EVH Edition',
+    img: './img/evh.png',
+    price: 500,
+  }, 
+  {
+    id:6,
+    top:'top',
+    category:'electric',
+    brand: 'Gibson SG',
+    img: './img/gibsonyoung.png',
+    price: 500,
+  }, 
+ 
+ 
 ]
